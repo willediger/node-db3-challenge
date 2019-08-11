a470db628b4ea860288d1e1d8711605ef3f5cc55
unknown <will.ediger@gmail.com>
2019-08-10 19:24:49 -0500

    added logger & errHandler

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const express = require('express');
+const express = require("express");
 
-const SchemeRouter = require('./schemes/scheme-router.js');
+const SchemeRouter = require("./schemes/scheme-router.js");
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/schemes', SchemeRouter);
+server.use(logger);
+
+server.use("/api/schemes", SchemeRouter);
+
+server.use(errHandler);
+
+function errHandler(err, req, res, next) {
+  res.status(err.status).json({ message: err.message });
+}
+
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+  next();
+}
 
 module.exports = server;
